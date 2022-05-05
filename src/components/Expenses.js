14b74f2462fc9e6337e9c8ebd0414e22de846d2b
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2022-05-05 22:37:38 +0530

    [Aparna]Add. Functionality to display the expense that got added.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,45 +1,50 @@
 import ExpenseItem from "./ExpenseItem";
 import {Grid} from "@material-ui/core";
-import React from "react";
+import React, {useState} from "react";
 import NewExpense from "./NewExpense";
 
 
 const Expenses = () => {
-    const expenses = [
+
+    const initialExpense = [
         {
-            id: '1',
+            id: 1,
             title: 'Grocery',
             expenseAmount: 100,
             date: new Date(2020, 7, 14),
         },
         {
-            id: '2',
+            id: 2,
             title: 'Petrol',
             expenseAmount: 200,
             date: new Date(2021, 2, 28),
         },
         {
-            id: '3',
+            id: 3,
             title: 'Electricity',
             expenseAmount: 300,
             date: new Date(2021, 5, 12),
         },
     ];
+    let idCount = 3;
+    let [expense,setExpense] = useState(initialExpense);
+
+
+
+    const addExpense = (newExpense) => {
+        newExpense.id = idCount+1;
+        idCount= idCount+1;
+        setExpense([...expense,newExpense])
+    }
     return (
         <>
             <Grid container direction="column" style={{padding: "15px"}}>
-                <NewExpense items={expenses}/>
-                <ExpenseItem title={expenses[0].title}
-                             expenseAmount={expenses[0].expenseAmount}
-                             date={expenses[0].date}/>
-
-                <ExpenseItem title={expenses[1].title}
-                             expenseAmount={expenses[1].expenseAmount}
-                             date={expenses[1].date}/>
-
-                <ExpenseItem title={expenses[2].title}
-                             expenseAmount={expenses[2].expenseAmount}
-                             date={expenses[2].date}/>
+                <NewExpense addExpense={addExpense}/>
+                {expense.map((expense) =>
+                    <ExpenseItem title={expense.title}
+                                 expenseAmount={expense.expenseAmount}
+                                 date={new Date(expense.date)}/>
+                )}
             </Grid>
         </>
     )
--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,10 +1,34 @@
 import {Button, Grid, Paper, TextField} from "@material-ui/core";
+import {useEffect, useState} from "react";
 
-const NewExpense = () => {
+const NewExpense = ({addExpense}) => {
+
+    const [inputFields, setInputFields] = useState({title: '', expenseAmount: '', date: new Date()});
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(event)
+        addExpense(inputFields);
+    }
+
+    const handleTitleChange = (event) => {
+        setInputFields({
+            ...inputFields,
+            title: event.target.value
+        })
+    }
+
+    const handleExpenseAmountChange = (event) => {
+        setInputFields({
+            ...inputFields,
+            expenseAmount: event.target.value
+        })
+    }
+
+    const handleDateChange = (event) => {
+        setInputFields({
+            ...inputFields,
+            date: event.target.value
+        })
     }
 
     return (
@@ -12,18 +36,23 @@ const NewExpense = () => {
             <Paper square style={{width: "400px", height: "200px"}}>
                 <form onSubmit={handleSubmit}>
                     <Grid container>
-                        <div style={{width:"200px"}}>Title : </div>
-                        <TextField  title="title" />
+                        <div style={{width: "200px"}}>Title :</div>
+                        <TextField title="title" onChange={handleTitleChange}/>
                     </Grid>
                     <Grid container>
-                        <div style={{width:"200px"}}>Expense Amount  : </div>
-                        <TextField  title="expense Amount"/>
+                        <div style={{width: "200px"}}>Expense Amount :</div>
+                        <TextField title="expense Amount" onChange={handleExpenseAmountChange}/>
                     </Grid>
-                    <Grid container >
-                        <div style={{width:"200px"}}>Date : </div>
-                        <TextField  title="date" type="date" />
+                    <Grid container>
+                        <div style={{width: "200px"}}>Date :</div>
+                        <TextField title="date" type="date" onChange={handleDateChange}/>
                     </Grid>
-                    <Button type="submit" size={"small"} style={{alignSelf:"center",margin:"12px",backgroundColor:"lightgreen",border:"1px solid green"}}>Submit</Button>
+                    <Button type="submit" size={"small"} style={{
+                        alignSelf: "center",
+                        margin: "12px",
+                        backgroundColor: "lightgreen",
+                        border: "1px solid green"
+                    }}>Submit</Button>
                 </form>
             </Paper>
         </>
